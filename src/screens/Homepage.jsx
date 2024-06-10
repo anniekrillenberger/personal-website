54e81b33a7976f3882d03f58fbb33610f3e6b931
anniekrillenberger <krillenberge@wisc.edu>
2024-06-09 22:05:39 -0500

    homepage design

--- src/screens/Homepage.jsx
+++ src/screens/Homepage.jsx
@@ -1,22 +1,54 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap'
 
-import me from '../images/me-wien.jpg'; 
+import me from '../images/me-wien-edited.jpg';
+import vk from '../images/output-onlinejpgtools.jpg'
+import vkHover from '../images/output-onlinejpgtools-hover.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Homepage() {
 
-    return (
-      <Container fluid>
-        <Row>
-          <Col md={6}>
-            <div className="image-container">
-              <img src={me} alt="Me in Vienna!" className="image-style"/>
+  const [isVkHovered, setIsVkHovered] = useState(false);
+
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col className="homepage">
+          <img src={me} alt="Me outside the Vienna Art Museum" className="image-style" />
+        </Col>
+        <Col className="homepage">
+          <div>
+            <h1>
+              <span className="homepage-hi">Hi! </span>
+              <span className="homepage-text">  My name's Annie :)</span>
+            </h1>
+            <p className="homepage-paragraph">I am a UW-Madison graduate working in software development based in Madison, Wisconsin. Thanks for coming to my corner of the internet. I'm glad you're here!</p>
+            <div className="social-icons">
+              <a href="https://www.linkedin.com/in/anne-krillenberger" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} size="4x" className="social-icon" />
+              </a>
+              <a href="https://github.com/anniekrillenberger" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} size="4x" className="social-icon" />
+              </a>
+              <a
+                href="https://www.valueknowledge.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                onMouseEnter={() => setIsVkHovered(true)}
+                onMouseLeave={() => setIsVkHovered(false)}
+              >
+                <img
+                  src={isVkHovered ? vkHover : vk}
+                  alt="Redirect to ValueKnowledge"
+                  className="social-icon vk-icon"
+                />
+              </a>
             </div>
-          </Col>
-          <Col md={6} className="d-flex justify-content-center align-items-center">
-            Hi, I'm Annie!
-          </Col>
-        </Row>
-      </Container>
-      );
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
