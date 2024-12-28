f09a5a988322d975fe47d3c55b0b6926ccc8c5a1
anniekrillenberger <krillenberge@wisc.edu>
2024-12-27 19:38:07 -0600

    api testing with render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,18 @@ import 'react-tooltip/dist/react-tooltip.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navigation from './Navigation.jsx';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
 
-  const [message, setMessage] = useState('');
+  const API_URL = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
-    axios.get('http://localhost:5000/test')
+    axios.get(`${API_URL}/test`)
       .then(response => {
         console.log("Message from API: ", response.data.message)
-        setMessage(response.data.message)
-   } )
+      })
       .catch(error => console.error('Error:', error));
   }, []);
 
--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
+require('dotenv').config();
 const express = require('express');
+const app = express();
 const cors = require('cors');
 
-const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+const API_KEY = process.env.API_KEY;
+
+// Enable CORS for your frontend (localhost during development)
+const allowedOrigins = ['http://localhost:3000', 'https://krillenberger.com'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 
 // Middleware
 app.use(cors());
