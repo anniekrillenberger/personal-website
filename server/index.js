f01b0aba799fc8cb583d06ca77a40fb94d63bc33
anniekrillenberger <krillenberge@wisc.edu>
2025-02-13 21:02:32 -0600

    PLEASE work on safariandedge PLEASE

--- server/index.js
+++ server/index.js
@@ -4,9 +4,13 @@ const cors = require('cors');
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.redirect(301, 'https://www.krillenberger.com');
-})
+app.use((req, res, next) => {
+    if (req.headers["x-forwarded-proto"] !== "https") {
+      return res.redirect(301, "https://" + req.headers.host + req.url);
+    }
+    next();
+  });
+  
 
 app.listen(8080, () => {
     console.log('server listening on port 8080.');
